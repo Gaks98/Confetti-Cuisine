5b49d2418323b446a14689aa234b841ef791ee32
Gaks98 <yvonnegax98@gmail.com>
2021-01-16 12:39:06 +0300

    add a series of routes

--- main.js
+++ main.js
@@ -4,3 +4,60 @@ const port = 3000,
   router = require('./router'),
   contentTypes = require('./contentTypes'),
   utils = require('./utils');
+
+//add a series of routes for web pages and assets
+router.get("./", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("/views/index.html", res);
+});
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("/views/index.html", res);
+});
+
+router.get("/course.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("/views/course.html", res);
+});
+
+router.get("/contact.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("/views/contact.html", res);
+});
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("/views/thanks.html", res);
+});
+
+router.get("/graph.png", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.png);
+  utils.getFile("public/images/graph.png", res);
+});
+
+router.get("/people.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/people.jpg", res);
+});
+
+router.get("/product.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/product.jpg", res);
+});
+
+router.get("/confetti_cuisine.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/confetti_cuisine.css", res);
+});
+
+router.get("/bootstrap.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/bootstrap.css", res);
+});
+
+router.get("/confetti_cuisine.js", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.js);
+  utils.getFile("public/js/confetti_cuisine.js", res);
+});
+
