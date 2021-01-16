91bfe91cd6a7ec19cb36a5efabb62b037e037796
Gaks98 <yvonnegax98@gmail.com>
2021-01-16 10:27:34 +0300

    export content type mapping object

---
+++ contentTypes.js
@@ -0,0 +1,21 @@
+// exporting an object that maps file types to their header values for use in my responses
+module.exports = {
+  html: {
+    "Content-Type": "text/html"
+  },
+  text: {
+    "Content-Type": "text/plain"
+  },
+  js: {
+    "Content-Type": "text/js"
+  },
+  jpg: {
+    "Content-Type": "text/jpg"
+  },
+  png: {
+    "Content-Type": "text/png"
+  },
+  css: {
+    "Content-Type": "text/css"
+  },
+};
