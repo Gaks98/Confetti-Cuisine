33aee32a8705c000543da1d3ecad89d17e6a0b18
Gaks98 <yvonnegax98@gmail.com>
2021-01-16 11:28:11 +0300

    create the get and post functions to map route functions

--- router.js
+++ router.js
@@ -20,4 +20,12 @@ exports.handle((req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.html);
     utils.getFile("views/error.html", res);
   }
-})
+});
+
+exports.get = (url, action) => {
+  routes["GET"][url] = action;
+};
+
+exports.post = (url, action) => {
+  routes["POST"][url] = action;
+};
