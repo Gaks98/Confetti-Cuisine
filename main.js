623d17c031bf500dac73eb02850d5eec00825c12
Gaks98 <yvonnegax98@gmail.com>
2021-02-14 14:36:26 +0300

    create a route for the home page

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const express = require('express'),
   app = express();
 
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to confetti cuisine!");
+});
+
