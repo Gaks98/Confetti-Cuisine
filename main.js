ada3ef3d57980a0b6845efb5c17310173a96ab31
Gaks98 <yvonnegax98@gmail.com>
2021-02-14 19:43:59 +0300

    add routes for courses,contact and contact form submission page

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to confetti cuisine!");
 });
 
+app.get('/courses', homeController.showCourses);
+app.get('/contact', homeController.showSignUp);
+app.get('/contact', homeController.postedSignUpForm);
+
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
 })
