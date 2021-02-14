e7730422fc47860e09ec6ac5c9b08dc2d77a9978
Gaks98 <yvonnegax98@gmail.com>
2021-02-14 14:47:37 +0300

    tell express app to use body-parser for processing URL-encoding and JSON parameters

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const express = require('express'),
   app = express();
 
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
